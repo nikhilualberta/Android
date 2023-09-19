ecdf15e2dd3f25de77e6453589905e2ee077611f
Nikhil <nikhilnayyar67@gmail.com>
2023-09-18 21:17:43 -0600

    worked on displaying list

--- nnayyar1ExpBook/app/src/main/java/com/example/nnayyar1_expbook/FirstFragment.java
+++ nnayyar1ExpBook/app/src/main/java/com/example/nnayyar1_expbook/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.nnayyar1_expbook;
 
+import android.icu.util.CurrencyAmount;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +9,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.nnayyar1_expbook.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private RecyclerView recyclerView;
+    private ExpenseAdapter adapter;
+    private ArrayList<ExpenseRecord> expenseRecords;
 
     @Override
     public View onCreateView(
@@ -29,11 +38,19 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        recyclerView = binding.recyclerView;
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        expenseRecords = new ArrayList<>();
+        expenseRecords.add(new ExpenseRecord("test", new Date(),123.0, "test"));
+        adapter = new ExpenseAdapter(expenseRecords);
+        recyclerView.setAdapter(adapter);
+
         binding.addExpense.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                        .navigate(R.id.expenseListToAddExpense);
             }
         });
     }
