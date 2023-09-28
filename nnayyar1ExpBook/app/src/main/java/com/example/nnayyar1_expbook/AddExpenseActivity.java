e368ab95932fb552c0b4db7756a769b78a598a54
Nikhil <nikhilnayyar67@gmail.com>
2023-09-27 20:10:24 -0600

    limited the number of decimal places to 2

--- nnayyar1ExpBook/app/src/main/java/com/example/nnayyar1_expbook/AddExpenseActivity.java
+++ nnayyar1ExpBook/app/src/main/java/com/example/nnayyar1_expbook/AddExpenseActivity.java
@@ -4,6 +4,11 @@ References:
  From: Stack Overflow
  Date of Publish: Aug 17, 2010
  URL: https://stackoverflow.com/questions/3500197/how-to-display-toast-in-android
+
+[2] for limiting decimal places on input
+ From: Tutorials point
+ Date of Publish: Jul 31, 2019
+ URL: https://www.tutorialspoint.com/how-to-limit-decimal-places-in-android-edittext
  */
 package com.example.nnayyar1_expbook;
 
@@ -11,12 +16,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AddExpenseActivity extends AppCompatActivity {
     private EditText expenseName;
@@ -37,8 +47,9 @@ public class AddExpenseActivity extends AppCompatActivity {
         yearPicker = findViewById(R.id.yearPicker);
         monthPicker = findViewById(R.id.monthPicker);
 
-        yearPicker.setMinValue(2010);
-        yearPicker.setMaxValue(2030);
+        expenseCharge.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(6, 2)});
+        yearPicker.setMinValue(2020);
+        yearPicker.setMaxValue(2025);
 
         monthPicker.setMinValue(1);
         monthPicker.setMaxValue(12);
@@ -71,5 +82,22 @@ public class AddExpenseActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+}
+
+class DecimalDigitsInputFilter implements InputFilter {
+    private Pattern mPattern;
+
+    public DecimalDigitsInputFilter(int digitsBeforeZero, int digitsAfterZero) {
+        mPattern = Pattern.compile("[0-9]{0," + (digitsBeforeZero - 1) + "}+((\\.[0-9]{0," + (digitsAfterZero - 1) + "})?)||(\\.)?");
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        Matcher matcher = mPattern.matcher(dest);
+        if (!matcher.matches()) {
+            return "";
+        }
+        return null;
     }
 }
